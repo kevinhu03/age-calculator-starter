e7133a44ed0d678278276a89b70526b7a675773a
Glitch (age-calculatorr) <none>
2020-12-17 00:36:37 +0000

    🌝🚇 Updated with Glitch

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-
+$("button").click(function() {
+  let birthYear = $(".year").val();
+  let name = $('.name').val();
+  let age = 2020 - birthYear;
+  
+  $(".message").text(`${name} is ${age} years old`);
+});
